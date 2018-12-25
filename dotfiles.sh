be505c96159f99a889dbed3d726459e320b2a73b
errandur <ethanschoultz@gmail.com>
2018-12-24 19:45:34 -0700

    Default

---
+++ dotfiles.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+### Update APT and install GIT ###
+##################################
+sudo apt -y update && sudo apt -y install git
+
+### Clone repo ###
+##################
+git clone https://github.com/errandur/dotfiles
+
+### Source new bashrc ###
+#########################
+&& echo 'source ~/dotfiles/.bashrc' > ~/.bashrc
+&& source ~/.bashrc
+
+### Install ranger ###
+######################
+sudo apt install -y ranger
+
+### Copy ranger config files ###
+################################
+ranger --copy-config=all
+
+### Set new Ranger config ###
+#############################
+sudo echo 'source ~/dotfiles/rc.conf' > ~/.config/ranger/rc.conf
+
+### Install Tmux ###
+####################
+sudo apt install -y tmux
+
+### Set new Tmux config ###
+###########################
+sudo touch ~/.tmux.conf
+&& echo 'source ~/dotfiles/.tmux.conf' > ~/.tmux.conf
+
