666bbbf858a6841d91740ed429b7972baaec499a
Errandur <40605977+Errandur@users.noreply.github.com>
2018-12-25 16:55:54 -0700

    Update dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,36 +1,45 @@
 #! /bin/bash
 
+##################################
 ### Update APT and install GIT ###
 ##################################
 sudo apt -y update && sudo apt -y install git
 
-### Clone repo ###
 ##################
+### Clone repo ###
 git clone https://github.com/errandur/dotfiles 
 
+#########################
 ### Source new bashrc ###
 #########################
 echo 'source ~/dotfiles/.bashrc' > ~/.bashrc
 
+######################
 ### Install ranger ###
 ######################
 sudo apt install -y ranger
 
+################################
 ### Copy ranger config files ###
 ################################
 ranger --copy-config=all
 
+#############################
 ### Set new Ranger config ###
 #############################
 sudo echo 'source ~/dotfiles/rc.conf' > ~/.config/ranger/rc.conf
 
+####################
 ### Install Tmux ###
 ####################
 sudo apt install -y tmux
 
+###########################
 ### Set new Tmux config ###
 ###########################
 echo 'source ~/dotfiles/.tmux.conf' > ~/.tmux.conf
 
+##########################
+### Completion Message ###
 echo Setup Complete! Please Re-Launch Terminal!
-###############################################
+##########################
